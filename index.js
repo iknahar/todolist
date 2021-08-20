7e1026af6f470a54cfd6351ae9f17d89f1a80392
Kamrun Nahar <knahar.kamrun@gmail.com>
2021-08-20 19:52:05 +0600

    task add

--- index.js
+++ index.js
@@ -6,7 +6,27 @@ const dayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday
 
 let dateObj = new Date();
 let myDate = ((dateObj.getUTCDate()+ " "+ monthNames[(dateObj.getMonth())] + " "+ dateObj.getUTCFullYear()+ ", "+ dayName[dateObj.getDay()] ));
-
 document.getElementById("date").innerText= myDate;
-console.log(dateObj.getUTCDate());
 
+
+// Date part done -------------------------
+
+
+document.getElementById("new-task-button").addEventListener("click", function(){
+let task = document.getElementById("inputBox").value;
+newTask = document.createElement("p");
+newTask.innerText = task; 
+
+let newDiv = document.createElement("div");
+newDiv.classList.add("task-box");
+newDiv.appendChild(newTask);
+
+console.log(newTask);
+
+const taskParent = document.getElementsByClassName("white-box")[0];
+taskParent.appendChild(newDiv);
+
+document.getElementById("inputBox").value = "";
+
+
+})
